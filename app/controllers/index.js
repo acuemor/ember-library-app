25894baf0d11a4da29c897c20b5889bd7abd55a4
Abel Cuevas <abel.cuevas.moreno@gmail.com>
2018-10-23 11:04:24 +0200

    Save email using save Promise

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,29 +1,34 @@
-import Controller from '@ember/controller';
-import { match, not } from '@ember/object/computed';
+import Controller from "@ember/controller";
+import { match, not } from "@ember/object/computed";
 
 export default Controller.extend({
-
-  emailAddress: '',
-  isValid: match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: not('isValid'),
-  headerMessage: 'Comming Soon',
+  emailAddress: "",
+  isValid: match("emailAddress", /^.+@.+\..+$/),
+  isDisabled: not("isValid"),
+  headerMessage: "Comming Soon",
 
   actions: {
-
     saveInvitation() {
       const email = this.emailAddress;
-      const newInvitation = this.store.createRecord('invitation', {email: email}); // Invitation es el modelo, y email es uno de los campos que tiene el modelo.
-      newInvitation.save();
-      
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
+      // Invitation es el modelo, y email es uno de los campos que tiene el modelo
+      const newInvitation = this.store.createRecord("invitation", {
+        email: email
+      });
+      newInvitation.save()
+      .then((response) => {
+        console.log("Email address is saved.", response);
+        this.set("responseMessage", `Thank you! We've just saved your email address: ${this.get("emailAddress")}`);
+        this.set("emailAddress", "");
+      })
+      .catch((reason) => {
+        console.error('Error con la promesa. Razón: ' + reason);
+      });
     }
   }
 
-    // Es más correcto poner: isDisabled: match('emailAddress'...), que es una computed property predefinida que existe
+  // Es más correcto poner: isDisabled: match('emailAddress'...), que es una computed property predefinida que existe
 
-//   isDisabled: computed('emailAddress', function() {
-//     return this.emailAddress === '';
-//   })
-
+  //   isDisabled: computed('emailAddress', function() {
+  //     return this.emailAddress === '';
+  //   })
 });
