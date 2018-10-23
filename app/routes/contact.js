9d0590834e1f816b03c00e6ad8afc3976a42fe2a
Abel Cuevas <abel.cuevas.moreno@gmail.com>
2018-10-23 13:00:53 +0200

    Refactor contacts to use model and firebase database

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,32 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
 export default Route.extend({
+  model() {
+    return this.store.createRecord("contact");
+  },
+
+  actions: {
+    saveContactForm(newContact) {
+      newContact
+        .save()
+        .then(() => {
+          this.controller.set(
+            "responseMessage",
+            `Thank you! We've just saved your form. Email: ${
+              newContact.email
+            } - Message: ${newContact.message}`
+          );
+          this.controller.set("model.email", "");
+          this.controller.set("model.message", "");
+        })
+        .catch(reason => {
+        //   console.error("Error con la promesa. Razón: " + reason);
+        });
+    },
+
+    willTransition() {
+      this.controller.get("model").rollbackAttributes();
+      this.controller.set("responseMessage", false);
+    }
+  }
 });
