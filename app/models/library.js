0210a0e0b8bd8f92b995750163350ba1b3beb055
Abel Cuevas <abel.cuevas.moreno@gmail.com>
2018-10-24 13:48:26 +0200

    Create admin/seeder route to manage multiple models in one route

--- app/models/library.js
+++ app/models/library.js
@@ -2,9 +2,14 @@ import DS from 'ember-data';
 import { notEmpty } from '@ember/object/computed'
 
 export default DS.Model.extend({
+  // Attrs
   name: DS.attr('string'),
   address: DS.attr('string'),
   phone: DS.attr('string'),
 
+  // Relations
+  books: DS.hasMany('book'),
+
+  // Validations
   isValid: notEmpty('name')
 });
